c75742d6b2ea0e4b392f3b08c91673bbd2f31c6b
Fryderyk <fryderyksv2@gmail.com>
2024-01-17 17:46:22 +0100

    Szkielet commit

---
+++ ZaliczenieNET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ZaliczenieNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
