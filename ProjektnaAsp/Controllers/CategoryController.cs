9c69b0b15f92f67a05512c5081debd91413d700b
Nikodem <nikodemus.mat@gmail.com>
2024-01-17 23:44:17 +0100

    Podstwawy

---
+++ ProjektnaAsp/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjektnaASP.Data;
+
+namespace ProjektnaASP.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext  db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var objCategoryList =  _db.Categories.ToList();
+            return View();
+        }
+    }
+}
